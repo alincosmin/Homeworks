0053f0bedbbac24a52cb455148fe8f1f52414b17
Alin <alincosmin123@gmail.com>
2015-02-28 18:21:50 +0200

    Terminat Tema 1 - CN

---
+++ CN/Tema1/Tema1/MatrixMultiplicator.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+
+namespace Tema1
+{
+    public class MatrixMultiplicator
+    {
+        private int _n;
+        private int[] _secondMatrixLines;
+        private bool[] _matrix1;
+        private bool[] _matrix2;
+
+        private IDictionary _sums;
+
+        public MatrixMultiplicator(int[] m1, int[] m2)
+        {
+            _n = (int) Math.Sqrt(m1.Length);
+            if (m1.Length != _n*_n || m1.Length != m2.Length)
+                throw new InvalidDataException("Matricile au dimensiuni incorecte");
+
+            _matrix1 = m1.Select(x => x != 0).ToArray();
+            _matrix2 = m2.Select(x => x != 0).ToArray();
+            _sums = new Dictionary<int, int>();
+            ComputeLines();
+        }
+
+        public int[] GetResult()
+        {
+            var result = new List<bool>();
+
+            for (int i = 0; i < _n; i++)
+            {
+                var line = _matrix1.Skip(i*_n).Take(_n).ToArray();
+                var computed = GetLineSumForLine(line);
+                result.AddRange(IntToBoolArray(computed));
+            }
+
+            var intResult = result.Select(x => x ? 1 : 0).ToArray();
+            return intResult;
+        }
+
+        private void ComputeLines()
+        {
+            _secondMatrixLines = new int[_n];
+            for (var i = 0; i < _n; i++)
+            {
+                var line = _matrix2.Skip(i*_n).Take(_n).ToArray();
+                _secondMatrixLines[i] = BoolArrayToInt(line);
+            }
+        }
+
+        private int GetLineSumForLine(bool[] line)
+        {
+            var intLine = BoolArrayToInt(line);
+            int sum = 0;
+            if (_sums.Contains(intLine))
+                sum = (int) _sums[intLine];
+            else
+            {
+                for (int i = 0; i < line.Length; i++)
+                {
+                    if (line[i])
+                        sum |= _secondMatrixLines[i];
+                }
+                _sums.Add(intLine, sum);
+            }
+
+            return sum;
+        }
+
+        // Utils
+        private int BoolArrayToInt(bool[] array)
+        {
+            var intcontainer = new int[1];
+            new BitArray(array.Reverse().ToArray()).CopyTo(intcontainer, 0);
+            return intcontainer[0];
+        }
+
+        private bool[] IntToBoolArray(int number)
+        {
+            return new BitArray(new int[] { number }).Cast<bool>().Take(_n).Reverse().ToArray();
+        }
+    }
+}
--- CN/Tema1/Tema1/Program.cs
+++ CN/Tema1/Tema1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace Tema1
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
@@ -18,6 +19,47 @@ namespace Tema1
             Console.WriteLine("Ex 2: a) {0}", assoc1);
             var ex = AssocMulExample(Math.Pow(10, -precision));
             Console.WriteLine("      b) (x * v) * v != x * (v * v) : x = {0}, v = 1+u", ex);
+
+            Console.WriteLine("Ex 3:");
+
+            var m1 = new int[]
+            {
+                1, 0, 1, 0,
+                1, 1, 0, 0,
+                1, 0, 0, 1,
+                0, 1, 0, 0
+            };
+
+            var m2 = new int[]
+            {
+                0, 0, 1, 1, 
+                1, 0, 0, 1,
+                0, 1, 1, 0, 
+                1, 0, 1, 0
+            };
+
+            var multiplicator = new MatrixMultiplicator(m1, m2);
+            var result = multiplicator.GetResult();
+
+            Console.WriteLine("M1:");
+            Console.WriteLine(m1.NiceString());
+            Console.WriteLine("M2:");
+            Console.WriteLine(m2.NiceString());
+            Console.WriteLine("Result:");
+            Console.WriteLine(result.NiceString());
+        }
+
+        public static string NiceString(this int[] matrix)
+        {
+            var n = (int)Math.Sqrt(matrix.Length);
+            var sb = new StringBuilder();
+            for (int i = 0; i < n; i++)
+            {
+                var line = matrix.Skip(i*n).Take(n).ToArray();
+                sb.AppendLine(string.Join(" ", line));
+            }
+
+            return sb.ToString();
         }
 
         public static int GetMachinePrecision()
@@ -44,9 +86,10 @@ namespace Tema1
             double p;
             double v = 1 + u;
             double x = 0xa7b7a7b7;
-            if (!((x*v)*v == x*(v*v)))
-                return x;
-            else return -1;
+            if ((x*v)*v == x*(v*v))
+                return -1;
+
+            return x;
         }
     }
 }
