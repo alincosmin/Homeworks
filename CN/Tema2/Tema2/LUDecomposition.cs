436e41eb38c53dbec96634a2bef92e2e54ce692f
Alin <alincosmin123@gmail.com>
2015-03-06 20:10:43 +0200

    [CN] Tema 2 - terminat descompunere LU

--- CN/Tema2/Tema2/LUDecomposition.cs
+++ CN/Tema2/Tema2/LUDecomposition.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Globalization;
 using System.IO;
+using System.Text;
 
 namespace Tema2
 {
@@ -7,35 +9,181 @@ namespace Tema2
     {
         private double _precision;
 
-        public int[] InitialMatrix { get; private set; }
-        public int[] Matrix { get; private set; }
+        public double[] InitialMatrix { get; private set; }
+        public double[] Matrix { get; private set; }
+
+        public double[] LMatrix
+        {
+            get
+            {
+                var matrix = new double[MatrixSize*MatrixSize];
+                for (int i = 0; i < MatrixSize; i++)
+                {
+                    matrix[i*MatrixSize + i] = 1;
+                }
+
+                for (int i = 1; i < MatrixSize; i++)
+                {
+                    for (int j = 0; j < i; j++)
+                    {
+                        matrix[i*MatrixSize + j] = Matrix[i*MatrixSize + j];
+                    }
+                }
+
+                return matrix;
+            }
+        }
+
+        public double[] UMatrix
+        {
+            get
+            {
+                var matrix = new double[MatrixSize * MatrixSize];
+                for (int i = 0; i < MatrixSize; i++)
+                {
+                    for (int j = i; j < MatrixSize; j++)
+                    {
+                        matrix[i * MatrixSize + j] = Matrix[i * MatrixSize + j];
+                    }
+                }
+
+                return matrix;
+            }
+        }
+
+        private bool _decomposed = false;
 
         public int MatrixSize { get; private set; }
 
-        public void SetMatrix(int[] matrix)
+        public void SetMatrix(double[] matrix)
         {
             var size = Math.Sqrt(matrix.Length);
             if (size == (int) size)
             {
                 InitialMatrix = matrix;
                 Matrix = matrix;
+                MatrixSize = (int) size;
             }
             else
                 throw new InvalidDataException("Invalid matrix size");
         }
 
-        public LUDecomposition(int negativePower)
+        public LUDecomposition(int precisionPower)
         {
-            _precision = Math.Pow(10, (-1)*negativePower);
+            _precision = Math.Pow(10, precisionPower);
         }
 
-        public object[] Decompose()
+        public double[] Decompose()
         {
-            return new object[]
+            double u, l;
+
+            if (!_decomposed)
+                for (int p = 0; p < MatrixSize; p++)
+                {
+                    for (int i = 0; i < MatrixSize; i++)
+                    {
+                        u = AGet(p, i);
+                        u -= GetSumForU(p, i);
+                        SetU(p, i, u);
+
+                        l = AGet(i, p);
+                        l -= GetSumForL(p, i);
+                        if (Math.Abs(UGet(p, p)) > _precision)
+                            l /= UGet(p, p);
+                        else throw new DivideByZeroException();
+                        SetL(i, p, l);
+                    }
+                    _decomposed = true;
+                }
+
+            return Matrix;
+        }
+
+        private double GetSumForU(int step, int index)
+        {
+            double sum = 0;
+
+            for (int k = 0; k < step; k++)
             {
-                new int[]{0},
-                new int[]{1}
-            };
+                sum += LGet(step, k)*UGet(k, index);
+            }
+
+            return sum;
+        }
+
+        private double GetSumForL(int step, int index)
+        {
+            double sum = 0;
+
+            for (int k = 0; k < step; k++)
+            {
+                sum += LGet(index, k)*UGet(k, step);
+            }
+
+            return sum;
+        }
+
+        private double AGet(int i, int j)
+        {
+            return Matrix[i*MatrixSize + j];
+        }
+
+        private double LGet(int i, int j)
+        {
+            double value = 0;
+
+            if (i >= j)
+            {
+                if (i == j)
+                    value = 1;
+                else value = AGet(i, j);
+            }
+
+            return value;
+        }
+
+        private double UGet(int i, int j)
+        {
+            double value = 0;
+
+            if (i <= j)
+                value = AGet(i, j);
+
+            return value;
+        }
+
+        private void SetL(int i, int j, double value)
+        {
+            if (i > j)
+                Matrix[i*MatrixSize + j] = value;
+        }
+
+        private void SetU(int i, int j, double value)
+        {
+            if (i <= j)
+                Matrix[i*MatrixSize + j] = value;
+        }
+    }
+
+    public static class MatrixExtensions
+    {
+        public static string NiceMatrixDisplay(this double[] matrix)
+        {
+            var sb = new StringBuilder();
+            var size = (int) Math.Sqrt(matrix.Length);
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    sb.Append(" ");
+                    sb.Append(matrix[i*size + j].ToString("F02"));
+                }
+
+                sb.AppendLine();
+            }
+
+            return sb.ToString();
         }
     }
 }
--- CN/Tema2/Tema2/Program.cs
+++ CN/Tema2/Tema2/Program.cs
@@ -1,18 +1,38 @@
-﻿namespace Tema2
+﻿using System;
+
+namespace Tema2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var LU = new LUDecomposition(15);
-            var matrix = new int[]
+            var lu = new LUDecomposition(-15);
+            var matrix = new double[]
             {
                 4, 2, 6,
                 2, 4, 6,
                 2, 2, 5
             };
 
-            LU.SetMatrix(matrix);
+            lu.SetMatrix(matrix);
+            /*
+             * expected result:
+             *   1   0  0    4 2 6
+             *  1/2  1  0    0 3 3
+             *  1/2 1/3 1    0 0 1
+             * 
+             */
+            var result = lu.Decompose();
+
+            Console.WriteLine("Initial matrix: ");
+            Console.WriteLine(matrix.NiceMatrixDisplay());
+            Console.WriteLine("\nLU matrix as one");
+            Console.WriteLine(result.NiceMatrixDisplay());
+            Console.WriteLine("\nL matrix");
+            Console.WriteLine(lu.LMatrix.NiceMatrixDisplay());
+            Console.WriteLine("\nU matrix");
+            Console.WriteLine(lu.UMatrix.NiceMatrixDisplay());
         }
+
     }
 }
